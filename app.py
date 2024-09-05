57724bf7ba5a4bc35cc69246591519f6e09ddc01
Agustin Martinez <amgil@arsys.es>
2024-09-05 18:11:22 +0200

    Unify HTML templates and change CSS

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from modules.symbol import Symbol
 
 # Set Python path to include the current directory
@@ -13,24 +13,21 @@ sys.dont_write_bytecode = True
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        symbols = request.form['symbols'].split(',')
+        stock_data = {symbol.strip(): Symbol(symbol.strip()) for symbol in symbols}
+        
+        gauge_charts = {symbol: stock_data[symbol].financial_metrics['TI_recommendations'] for symbol in stock_data}
 
+        for symbol_obj in stock_data.values():
+            symbol_obj.generate_stock_overview()
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    symbols = request.form['symbols'].split(',')
-    stock_data = {symbol.strip(): Symbol(symbol.strip()) for symbol in symbols}  # Create Symbol objects
-
-    # Generate gauge charts for each symbol
-    gauge_charts = {symbol: stock_data[symbol].financial_metrics['TI_recommendations'] for symbol in stock_data}
-
-    for symbol_obj in stock_data.values():
-        symbol_obj.generate_stock_overview()
-
-    return render_template('results.html', stock_data=stock_data, gauge_charts=gauge_charts)
-
+        return render_template('home.html', stock_data=stock_data, gauge_charts=gauge_charts)
+    else:
+        # For GET requests, just show an empty home page
+        return render_template('home.html', stock_data={}, gauge_charts={})
 
 if __name__ == '__main__':
     app.run(debug=True)
